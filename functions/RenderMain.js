6d040c3d09fc023ba1f1a24a8145f926c71b5061
= <=>
2022-10-20 13:04:11 -0300

    Optimización del codigo

--- functions/RenderMain.js
+++ functions/RenderMain.js
@@ -1,10 +1,10 @@
 function RenderMain(url){
-    if (url === "/index.html"){
+    if (url.includes("/index.html")){
         const main = document.createElement('main');
         document.getElementById('body').appendChild(main);
         main.innerHTML = Main();
     } if
-        (url === "/buyTickets.html"){
+        (url.includes("/buyTickets.html")){
             const main = document.createElement('main');
             document.getElementById('body').appendChild(main);
             main.innerHTML = BuyTickets();
